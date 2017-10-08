810af7eeae59c50fc8b276f08be7ecdb13907b93
Miguel Palau <mpalauzarza@gmail.com>
2017-10-07 21:24:23 -0500

    adding some loving examples

--- gulp/tasks/markup.js
+++ gulp/tasks/markup.js
@@ -3,7 +3,14 @@ const config = require('../config')
 const $ = require('gulp-load-plugins')()
 const production = config.production
 const fs = require('fs')
-const blur = require('./blur')
+
+// Performance my dude
+const {cwd} = require('process')
+const BlurryPug = require('lqip-pug')
+const blurry = new BlurryPug({
+  baseDir: cwd() + '/' + config.directories.src.images,
+  compiledPath: 'assets/images'
+})
 
 gulp.task('markup', () =>
   gulp.src(config.directories.src.markup + '/*.pug')
@@ -12,7 +19,7 @@ gulp.task('markup', () =>
       locals: {
         icon: name => fs.readFileSync(`./src/assets/icons/${name}.svg`),
         production,
-        blur
+        blur: name => blurry.blur(name)
       }
     })).on('error', config.onError)
     .pipe($.htmlPrettify())
