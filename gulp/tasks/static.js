e071984e86a53792d4c2e1f13206c36230f2e8cc
Miguel Palau <mpalauzarza@gmail.com>
2017-10-08 01:06:43 -0500

    cleanup the css

--- gulp/tasks/static.js
+++ gulp/tasks/static.js
@@ -6,8 +6,11 @@ gulp.task('clean', () =>
   del([config.directories.dist.base])
 )
 
+gulp.task('clean:css', () =>
+  del([config.directories.dist.styles])
+)
+
 gulp.task('images', () =>
   gulp.src(config.directories.src.images + '/**/*')
     .pipe(gulp.dest(config.directories.dist.images))
 )
-
