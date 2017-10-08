31e8cefece5950a521f7397e30ad6ff94006ccfe
Miguel Palau <mpalauzarza@gmail.com>
2017-10-08 01:22:24 -0500

    more optimal

--- src/assets/js/general/index.js
+++ src/assets/js/general/index.js
@@ -1,10 +1,11 @@
 const FontFaceObserver = require('fontfaceobserver')
 const link = document.createElement('link')
-link.href = 'https://fonts.googleapis.com/css?family=Fira+Mono'
 link.rel = 'stylesheet'
+link.href = 'https://fonts.googleapis.com/css?family=Fira+Mono'
+link.setAttribute('async', 'true')
 document.head.appendChild(link)
 // When the fonts are loaded append class to the html
-const roboto = new FontFaceObserver('Roboto')
+const roboto = new FontFaceObserver('Fira Mono')
 roboto.load().then(() => {
   document.documentElement.classList.add('wf-active')
 })
