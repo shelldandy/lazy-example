033edd0692a4f4fb38c11d1724764934ce652aa3
Miguel Palau <mpalauzarza@gmail.com>
2017-10-07 22:39:42 -0500

    reducing just a bit the artificial delay

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -7,5 +7,5 @@ loopQuery(images, (index, image) => {
   const src = image.getAttribute('data-src')
   setTimeout(() => {
     image.src = src
-  }, Math.floor(Math.random() * (2000 + 1) + 500))
+  }, Math.floor(Math.random() * (1000 + 1) + 500))
 })
