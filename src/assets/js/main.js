4d0105d3f70b3ea2cdb57c42e46dd8e44dd0abab
Miguel Palau <mpalauzarza@gmail.com>
2017-10-07 22:16:48 -0500

    clean console logs

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,7 +5,6 @@ const images = document.querySelectorAll('img')
 
 loopQuery(images, (index, image) => {
   const src = image.getAttribute('data-src')
-  console.log(src)
   setTimeout(() => {
     image.src = src
   }, Math.floor(Math.random() * (2000 + 1) + 500))
